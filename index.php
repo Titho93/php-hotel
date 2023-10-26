17f9e71e203ee1811b8e389b75702d4468e95fd8
titho93 <titho93.10@gmail.com>
2023-10-26 15:34:12 +0200

    [add]Tab

--- index.php
+++ index.php
@@ -45,6 +45,8 @@ $hotels = [
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
@@ -61,5 +63,31 @@ $hotels = [
         </li>
         <?php endforeach; ?>
     </ul>
+
+    <h2>TAB</h2>
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Parking</th>
+      <th scope="col">Vote</th>
+      <th scope="col">Distance to Centre</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach($hotels as $key => $hotel): ?>
+    <tr>
+      <td><?php echo $hotel['name'] ?></td>
+      <td><?php echo $hotel['description'] ?></td>
+      <td><?php echo ($hotel['parking'] === true) ? "yes" : "no"; ?></td>
+      <td><?php echo $hotel['vote'] ?></td>
+      <td><?php echo $hotel['distance_to_center'] ?> km</td>
+    </tr>
+
+    <?php endforeach; ?>
+  </tbody>
+</table>
 </body>
 </html>
